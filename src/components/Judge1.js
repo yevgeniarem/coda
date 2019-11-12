e2e65e7d73cd4cf609657fbf5a7b88cb7553c5ac
Yevgenia Rem <yevgeniarem@gmail.com>
2019-11-12 13:14:25 -0800

    trying to commit again

---
+++ src/components/Judge1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Judge1() {
+  return (
+    <div >
+        <h1>Judge 1</h1>
+        {/* <img src='../../public/images/coda.svg' alt='CODA logo'></img> */}
+    </div>
+  );
+}
+
+export default Judge1;
