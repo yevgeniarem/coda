f6b636ea1459c7fd3e0480ac0ddc021367406af4
Yevgenia Rem <yevgeniarem@gmail.com>
2019-11-12 13:13:16 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Judge1 from './components/Judge1';
+import Judge2 from './components/Judge2';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/'>
+                <Judge1 />
+            </Route>
+            <Route path='/Judge2'>
+                <Judge2 />
+            </Route>
+        </div>
+    </Router>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
