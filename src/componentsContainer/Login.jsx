0bfd99bcce63d78f0a094d3e33c57305bb476169
Yevgenia Rem <yevgeniarem@gmail.com>
2020-11-07 13:27:02 -0800

    added welcome modal

--- src/componentsContainer/Login.jsx
+++ src/componentsContainer/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Image, Form } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
@@ -6,8 +6,9 @@ import { useForm } from 'react-hook-form';
 
 import InputGroup from '../componentsReusable/InputGroup';
 import SubmitButton from '../componentsReusable/buttons/SubmitButton';
-import { tryLogin } from '../redux/actions/appActions';
+import { tryLogin, runModal } from '../redux/actions/appActions';
 import { formInputs } from '../utils/constants';
+import { Modal } from '../utils/models';
 
 export default function Login() {
   const history = useHistory();
@@ -16,6 +17,25 @@ export default function Login() {
     validateCriteriaMode: 'all',
   });
   const { name, password } = useSelector((state) => state.login);
+
+  useEffect(() => {
+    dispatch(
+      runModal(
+        new Modal({
+          isModalShown: true,
+          modalInfo: {
+            title: 'Welcome to my CODA app!',
+            body:
+              'Use "jane" and "testtest" to log in. Due to COVID, live events have been postponed, and data may be incomplete as adjustments are being made. Regardless, feel free to look around, and thanks for stopping by!',
+            confirm: {
+              text: 'Ok!',
+            },
+          },
+        }),
+      ),
+    );
+    // eslint-disable-next-line
+  }, []);
 
   const onSubmit = async () => {
     await dispatch(tryLogin({ name, password }));
